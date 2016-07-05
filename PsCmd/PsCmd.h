e742f2cbcd85aa5b9db2650f575ac988dda1ab63
Yoshio HANAWA <y@hnw.jp>
2016-07-06 01:50:12 +0900

    Initial commit

---
+++ PsCmd/PsCmd.h
@@ -0,0 +1,19 @@
+//
+//  PsCmd.h
+//  PsCmd
+//
+//  Created by hnw on 2016/07/06.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PsCmd.
+FOUNDATION_EXPORT double PsCmdVersionNumber;
+
+//! Project version string for PsCmd.
+FOUNDATION_EXPORT const unsigned char PsCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PsCmd/PublicHeader.h>
+
+
